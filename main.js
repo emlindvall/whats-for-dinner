000dfc5010616558242a9fbe8ab1fc09ac3c6f21
Em Lindvall <elindvall22@turing.edu>
2023-02-15 23:53:36 -0700

    Add meal type arrays

--- main.js
+++ main.js
@@ -5,7 +5,55 @@ var crockpotImage = document.querySelector('.crockpot-image');
 var recipeContainer = document.querySelector('.recipe-container');
 
 
+var sides = [
+    "Miso Glazed Carrots",
+    "Coleslaw",
+    "Garden Salad",
+    "Crispy Potatoes",
+    "Sweet Potato Tots",
+    "Coconut Rice",
+    "Caeser Salad",
+    "Shrimp Summer Rolls",
+    "Garlic Butter Mushrooms",
+    "Hush Puppies"
+]
 
+var mains = [
+    "Spaghetti and Meatballs",
+    "Pineapple Chicken",
+    "Shakshuka",
+    "Thai Yellow Curry",
+    "Bibimbap",
+    "Chicken Parmesean",
+    "Butternut Squash Soup",
+    "BBQ Chicken Burgers",
+    "Ramen",
+    "Empanadas",
+    "Chicken Fried Rice",
+    "Sheet Pan Fajitas",
+    "Margarita Pizza"
+]
+
+var desserts = [
+    "Apple Pie",
+    "Lemon Meringue Pie",
+    "Black Forest Cake",
+    "Banana Bread",
+    "Peach Cobbler",
+    "Cheesecake",
+    "Funfetti Cake",
+    "Baklava",
+    "Flan",
+    "Macarons",
+    "Macaroons",
+    "Chocolate Cupcakes",
+    "Pavlova",
+    "Pumpkin Pie",
+    "Key Lime Pie",
+    "Tart Tatin",
+    "Croissants",
+    "Eclairs"
+]
 
 
 
@@ -16,15 +64,6 @@ var recipeContainer = document.querySelector('.recipe-container');
 
 letsCookButton.addEventListener('click', getRandomRecipe);
 
-
-
-const hide = (element) => {
-    element.classList.add('hidden');
-}
-  
-const show = (element) => {
-    element.classList.remove('hidden');
-}
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length);
